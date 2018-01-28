e46974c18d526402e67c89ce20122f4d330fbfe7
Aakash Verma <aku06_ran@yahoo.co.in>
2018-01-28 22:50:05 +0530

    Modified ionode.js to pass learnyounode

--- io/ionode.js
+++ io/ionode.js
@@ -1,3 +1,4 @@
 const fs = require('fs');
-
+// Uncomment below for learnyounode
+// console.log(fs.readFileSync(process.argv[2]).toString().match(/\n/g).length);
 module.exports = filePath => fs.readFileSync(filePath).toString().match(/\n/g);
