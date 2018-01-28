15f39e4c3aabe6f797ef583a23c791895e9f6ed1
Aakash Verma <aku06_ran@yahoo.co.in>
2018-01-28 22:39:17 +0530

    Modified previous tests and added test for empty file

--- io/io.test.js
+++ io/io.test.js
@@ -4,11 +4,16 @@ describe('testing file i/o', () => {
   it('testing with input file containing random text', () => {
     const fileInput = './test1.txt';
     const result = 3;
-    expect(ionode(fileInput)).toBe(result);
+    expect(ionode(fileInput).length).toBe(result);
   });
   it('testing with input file only 1 line feed', () => {
     const fileInput = './test2.txt';
     const result = 1;
+    expect(ionode(fileInput).length).toBe(result);
+  });
+  it('testing with input file no line feed', () => {
+    const fileInput = './test3.txt';
+    const result = null;
     expect(ionode(fileInput)).toBe(result);
   });
 });
--- io/ionode.js
+++ io/ionode.js
@@ -1,3 +1,3 @@
 const fs = require('fs');
 
-module.exports = filePath => fs.readFileSync(filePath).toString().match(/\n/g).length;
+module.exports = filePath => fs.readFileSync(filePath).toString().match(/\n/g);
