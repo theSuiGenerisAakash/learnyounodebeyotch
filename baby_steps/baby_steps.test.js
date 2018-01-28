e333547d701ba065236e9422bb0b55719a8a9b08
Aakash Verma <aku06_ran@yahoo.co.in>
2018-01-28 23:36:09 +0530

    Added test for NaN as param in babysteps.js

--- baby_steps/baby_steps.test.js
+++ baby_steps/baby_steps.test.js
@@ -1,9 +1,15 @@
 const babysteps = require('./babysteps');
 
 describe('testing babysteps.js', () => {
-  it('testing with string parameters', () => {
+  it('testing with no parameters', () => {
     console.log = jest.fn(console.log);
     babysteps();
     expect(console.log).toHaveBeenCalledWith(0);
+  });
+
+  it('testing with NaN', () => {
+    console.log = jest.fn(console.log);
+    babysteps(NaN, 2);
+    expect(console.log).toHaveBeenCalledWith(NaN);
   });
 });
--- baby_steps/babysteps.js
+++ baby_steps/babysteps.js
@@ -1,3 +1,5 @@
 module.exports = (...args) => {
   console.log(args.reduce((accum, elem) => accum + Number(elem), 0));
 };
+
+// console.log(process.argv.slice(2).reduce((accum, elem) => accum + Number(elem), 0));
