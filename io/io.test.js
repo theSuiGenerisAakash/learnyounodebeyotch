b1142c04db9a0d0d8b049bef22c310f7e20fd5e4
Aakash Verma <aku06_ran@yahoo.co.in>
2018-01-28 22:33:51 +0530

    Added test with file having only one line feed

--- io/io.test.js
+++ io/io.test.js
@@ -6,4 +6,9 @@ describe('testing file i/o', () => {
     const result = 3;
     expect(ionode(fileInput)).toBe(result);
   });
+  it('testing with input file only 1 line feed', () => {
+    const fileInput = './test2.txt';
+    const result = 1;
+    expect(ionode(fileInput)).toBe(result);
+  });
 });
