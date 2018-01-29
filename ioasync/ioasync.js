ab8ead83b30ad73f83d8c2cef5c62dc791f680c8
Aakash Verma <aku06_ran@yahoo.co.in>
2018-01-30 00:00:46 +0530

    Improved tests and added callback logic test for ioasync.js

--- ioasync/ioasync.js
+++ ioasync/ioasync.js
@@ -1,4 +1,8 @@
 const fs = require('fs');
 
 // fs.readFile(process.argv[2], (err, data) => (console.log(data.toString().match(/\n/g).length)));
-module.exports = filePath => fs.readFile(filePath, (err, data) => console.log(data.toString()));
+module.exports = (filePath, cbk) => fs.readFile(filePath, (err, data) => {
+  const files = data.toString().match(/\n/g);
+  if (files !== null) { return cbk(files.length); }
+  return cbk(0);
+});
--- ioasync/ioasync.test.js
+++ ioasync/ioasync.test.js
@@ -3,27 +3,24 @@ const ioasync = require('./ioasync');
 describe('testing the asynchronous nature of readFile', () => {
   it('test with a math operation', () => {
     const filePath = './numFile.txt';
-    const oneNum = 2 + Number(ioasync(filePath));
+    const oneNum = 2 + Number(ioasync(filePath, () => {}));
     const result = NaN;
     expect(oneNum).toBe(result);
   });
 
   it('testing function return type immediately', () => {
     const filePath = './strFile.txt';
-    const strType = typeof ioasync(filePath);
+    const strType = typeof ioasync(filePath, () => {});
     const resultType = 'string';
     expect(strType).not.toBe(resultType);
   });
 
-  it('testing function execution order', () => {
-    const filePath = './numFile.txt';
-    console.log = jest.fn(console.log);
-    ioasync(filePath);
-    for (let i = 0; i < 10; i += 1) {
-      console.log(i);
-    }
-    // setTimeout(() => console.log('hello'), 0); Why doesn't this print anuthing?
-    expect(console.log).toHaveBeenCalledTimes(10);
-    // setTimeout(() => expect(console.log).toHaveBeenCalledTimes(11), 0);
+  it('testing callback logic', (done) => {
+    const filePath = '../io/test1.txt';
+    const cbk = (data) => {
+      expect(data).toBe(3);
+      done();
+    };
+    ioasync(filePath, cbk);
   });
 });
