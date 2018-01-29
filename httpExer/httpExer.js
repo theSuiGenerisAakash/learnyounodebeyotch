3fac26248e9bf0ad4ecce5a5fb202532490d4f4d
Aakash Verma <aku06_ran@yahoo.co.in>
2018-01-30 00:57:13 +0530

    added code for learnyounode in httpExer.js

--- httpExer/httpExer.js
+++ httpExer/httpExer.js
@@ -6,3 +6,8 @@ module.exports = (url, cbk) => {
     return cbk(res.statusCode);
   }).on('error', e => cbk(e.code));
 };
+
+// http.get(process.argv[2], (res) => {
+//   res.setEncoding('utf-8');
+//   res.on('data', datum => console.log(datum));
+// });
