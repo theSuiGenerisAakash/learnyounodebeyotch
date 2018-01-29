54613d79c6e442fcc44ad2fb3df83a41ea7aacc2
Aakash Verma <aku06_ran@yahoo.co.in>
2018-01-30 01:46:40 +0530

    Added code for learnyounode - httpCollect.js

--- httpCollect/httpCollect.js
+++ httpCollect/httpCollect.js
@@ -7,3 +7,12 @@ module.exports = (url, cbk) => {
     res.on('end', () => cbk([stringToBeRecvd, stringToBeRecvd.length]));
   });
 };
+//
+// let stringToBeRecvd = '';
+// http.get(process.argv[2], (res) => {
+//   res.on('data', datum => (stringToBeRecvd += datum.toString()));
+//   res.on('end', () => {
+//     console.log(stringToBeRecvd.length);
+//     console.log(stringToBeRecvd);
+//   });
+// });
